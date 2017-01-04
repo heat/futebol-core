b1323f8dea57e49ea0eadb983e0403ae7d4535a7
mikefarias <mikesantosfarias@gmail.com>
2017-01-04 11:00:38 -0300

    Entidade e repositório para acesso as ODDs cadastradas no banco

--- app/models/apostas/Odd.java
+++ app/models/apostas/Odd.java
@@ -14,9 +14,6 @@ public class Odd implements Serializable{
     @Column(name = "odds_odd_id",updatable = false)
     private Long id;
 
-    @Column(name = "tenant_id")
-    private Long tenant;
-
     @Column(name = "nome")
     private String nome;
 
@@ -42,10 +39,71 @@ public class Odd implements Serializable{
     @JoinColumn(name = "odd_id")
     private List<Taxa> taxas;
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
+    public String getMercado() {
+        return mercado;
+    }
 
+    public void setMercado(String mercado) {
+        this.mercado = mercado;
+    }
 
+    public char getTipoLinha() {
+        return tipoLinha;
+    }
 
+    public void setTipoLinha(char tipoLinha) {
+        this.tipoLinha = tipoLinha;
+    }
+
+    public String getAbreviacao() {
+        return abreviacao;
+    }
+
+    public void setAbreviacao(String abreviacao) {
+        this.abreviacao = abreviacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Long getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Long prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public Long getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(Long posicao) {
+        this.posicao = posicao;
+    }
+
+    public List<Taxa> getTaxas() {
+        return taxas;
+    }
+
+    public void setTaxas(List<Taxa> taxas) {
+        this.taxas = taxas;
+    }
 }
---
+++ app/repositories/OddRepository.java
@@ -0,0 +1,86 @@
+package repositories;
+
+
+import com.google.inject.Inject;
+import models.apostas.Odd;
+import models.vo.Confirmacao;
+import models.vo.Tenant;
+import play.db.jpa.JPAApi;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+public class OddRepository implements Repository<Long, Odd>{
+
+    JPAApi jpaApi;
+
+    @Inject
+    public OddRepository(JPAApi jpaApi) {
+
+        this.jpaApi = jpaApi;
+    }
+
+    @Override
+    public List<Odd> todos(Tenant tenant) {
+
+        EntityManager em = jpaApi.em();
+        Query query = em.createQuery("FROM Odd as o ");
+        return query.getResultList();
+    }
+
+    @Override
+    public Optional<Odd> buscar(Tenant tenant, Long id) {
+
+        try {
+            EntityManager em = jpaApi.em();
+            Odd odd = em.find(Odd.class, id);
+            return Optional.ofNullable(odd);
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public CompletableFuture<Odd> atualizar(Tenant tenant, Long id, Odd o) {
+
+        EntityManager em = jpaApi.em();
+        Optional<Odd> oddOptional = buscar(tenant, id);
+        if(!oddOptional.isPresent())
+            throw new NoResultException("Odd não encontrada");
+
+        Odd odd = oddOptional.get();
+        odd.setAbreviacao(o.getAbreviacao());
+        odd.setDescricao(o.getAbreviacao());
+        odd.setMercado(o.getMercado());
+        odd.setNome(o.getNome());
+        odd.setPosicao(o.getPosicao());
+        odd.setPrioridade(o.getPrioridade());
+        odd.setTipoLinha(o.getTipoLinha());
+        em.merge(odd);
+        return CompletableFuture.completedFuture(odd);
+    }
+
+    @Override
+    public CompletableFuture<Odd> inserir(Tenant tenant, Odd odd) {
+
+        EntityManager em = jpaApi.em();
+        em.persist(odd);
+        return CompletableFuture.completedFuture(odd);
+    }
+
+    @Override
+    public CompletableFuture<Confirmacao> excluir(Tenant tenant, Long id) {
+
+        EntityManager em = jpaApi.em();
+        Optional<Odd> oddOptional = buscar(tenant, id);
+        if(!oddOptional.isPresent())
+            throw new NoResultException("Odd não encontrada");
+        else em.remove(oddOptional.get());
+
+        return CompletableFuture.completedFuture(Confirmacao.CONCLUIDO);
+    }
+}
