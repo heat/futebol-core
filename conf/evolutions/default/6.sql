0ad447a0be179e92841e826d1a2ef10b517a61a1
wfgomes <wesckley.gomes@gmail.com>
2017-03-06 16:02:57 -0300

    Separando as novas tabelas no 6.sql

---
+++ conf/evolutions/default/6.sql
@@ -0,0 +1,50 @@
+# Bilhete domain schema
+
+# --- !Ups
+
+CREATE SEQUENCE public.contas_conta_id_seq;
+
+CREATE TABLE public.contas (
+                conta_id INTEGER NOT NULL DEFAULT nextval('public.contas_conta_id_seq'),
+                usuario_id INTEGER NOT NULL,
+                CONSTRAINT contas_pk PRIMARY KEY (conta_id)
+);
+
+ALTER TABLE public.contas ADD CONSTRAINT contas_usuarios_fk
+FOREIGN KEY (usuario_id)
+REFERENCES public.usuarios (usuario_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+CREATE SEQUENCE public.lancamentos_lancamento_id_seq;
+
+CREATE TABLE public.lancamentos (
+                lancamento_id INTEGER NOT NULL DEFAULT nextval('public.lancamentos_lancamento_id_seq'),
+                conta_id INTEGER NOT NULL,
+                valor DECIMAL(10,2) NOT NULL,
+                saldo DECIMAL(10,2) NOT NULL,
+                dtype VARCHAR(50) NOT NULL,
+                criado_em TIMESTAMP NOT NULL,
+		origem_bilhete INTEGER,
+                CONSTRAINT lancamentos_pk PRIMARY KEY (lancamento_id)
+);
+
+ALTER TABLE public.lancamentos ADD CONSTRAINT lancamentos_contas_fk
+FOREIGN KEY (conta_id)
+REFERENCES public.contas (conta_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+
+# --- !Downs
+
+DROP TABLE IF EXISTS lancamentos CASCADE;
+
+DROP TABLE IF EXISTS contas CASCADE;
+
+DROP SEQUENCE IF EXISTS public.lancamentos_lancamento_id_seq CASCADE;
+
+DROP SEQUENCE IF EXISTS public.contas_conta_id_seq CASCADE;
+
