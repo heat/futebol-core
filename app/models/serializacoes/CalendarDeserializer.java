fcedd95075e5889cedbc0175a22af22e4d08c9b3
wfgomes <wesckley.gomes@gmail.com>
2017-03-29 15:44:01 -0300

    Ajustando formato de data no deserialize

--- app/models/serializacoes/CalendarDeserializer.java
+++ app/models/serializacoes/CalendarDeserializer.java
@@ -3,6 +3,9 @@ package models.serializacoes;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -11,7 +14,7 @@ import java.util.Date;
 
 public class CalendarDeserializer extends JsonDeserializer<Calendar> {
 
-    private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 
 
     @Override
@@ -25,9 +28,11 @@ public class CalendarDeserializer extends JsonDeserializer<Calendar> {
 
     public Calendar deserialize(String dataEvento) throws IOException {
         try {
-            Date date = formatter.parse(dataEvento);
+            DateTimeFormatter fmt = ISODateTimeFormat.dateTime();
+            DateTime str = fmt.parseDateTime(dataEvento);
+            //Date date = formatter.parse(dataEvento);
             Calendar calendar = Calendar.getInstance();
-            calendar.setTime(date);
+            calendar.setTime(str.toDate());
             return calendar;
         } catch (Exception e) {
             throw new IOException(e);
